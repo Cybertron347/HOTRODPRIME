8be362088af3bf79d2edf4a78dab598d0334d15d
Luis.Suarez <Luis.Suarez@LT439.iasadmin.com>
2021-03-18 10:54:12 -0400

    HEY MY PARTY PEOPLE HERE IS MY PART OF THE TESTING i DIDI PLEASE REVIEW AND APPROVE THANK YOU

---
+++ NAVEENAUTOMATION/src/test/java/com/ias/qa1/TESTEXECUTION.java
@@ -0,0 +1,20 @@
+package com.ias.qa1;
+
+import org.junit.Test;
+
+public class TESTEXECUTION {
+
+	
+	@Test
+	public void test1() {
+		
+		
+		
+		System.out.println("HELLO MY IAS PEEPS");
+		System.out.println("HELLO MY IAS PEEPS");
+		System.out.println("HELLO MY IAS PEEPS");
+		System.out.println("HELLO MY IAS PEEPS");
+	}
+	
+	
+}
